be30f84a968b2eace4aec144b296e74ec22fd78b
Kevin Bauer <urupica@gmail.com>
2024-03-22 11:00:51 -0400

    update template

--- template.py
+++ template.py
@@ -1,16 +1,13 @@
 def main(filename):
-    result_part_1 = None
-    result_part_2 = None
     with open(filename) as input_file:
         for line in input_file:
             line = line.strip()
 
-            # part 1
-
-            # part 2
-
+    result_part_1 = None
     print(f"part 1: {result_part_1}")
-    print(f"part 2: {result_part_2}")
+
+    # result_part_2 = None
+    # print(f"part 2: {result_part_2}")
 
 
 if __name__ == "__main__":
