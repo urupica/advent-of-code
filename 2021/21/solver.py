b713df912461677ef1d9f1f70378fce3622597d3
Kevin Bauer <urupica@gmail.com>
2024-05-15 08:27:44 -0400

    improve 2021/21 sollution

--- 2021/21/solver.py
+++ 2021/21/solver.py
@@ -1,29 +1,31 @@
 from functools import cache
 
+DICE_SUM_DISTRIBUTION = {
+    3: 1,
+    4: 3,
+    5: 6,
+    6: 7,
+    7: 6,
+    8: 3,
+    9: 1,
+}
+
 
 @cache
-def compute_wins(pos_a, pos_b, total_a=0, total_b=0, turn=0):
+def compute_wins(pos_a, pos_b, total_a=0, total_b=0, turn="a"):
     if total_a >= 21:
         return 1, 0
     if total_b >= 21:
         return 0, 1
 
     total_wins_a = total_wins_b = 0
-    for steps, count in {
-        3: 1,
-        4: 3,
-        5: 6,
-        6: 7,
-        7: 6,
-        8: 3,
-        9: 1,
-    }.items():
-        if turn == 0:
-            new_pos = (pos_a + steps) % 10
-            wins_a, wins_b = compute_wins(new_pos, pos_b, total_a + new_pos + 1, total_b, 1)
+    for steps, count in DICE_SUM_DISTRIBUTION.items():
+        if turn == "a":
+            new_pos_a = (pos_a + steps) % 10
+            wins_a, wins_b = compute_wins(new_pos_a, pos_b, total_a + new_pos_a + 1, total_b, "b")
         else:
-            new_pos = (pos_b + steps) % 10
-            wins_a, wins_b = compute_wins(pos_a, new_pos, total_a, total_b + new_pos + 1, 0)
+            new_pos_b = (pos_b + steps) % 10
+            wins_a, wins_b = compute_wins(pos_a, new_pos_b, total_a, total_b + new_pos_b + 1, "a")
         total_wins_a += count * wins_a
         total_wins_b += count * wins_b
     return total_wins_a, total_wins_b
@@ -36,11 +38,12 @@ def main(filename):
             for i, line in enumerate(input_file, start=1)
         ]
 
+    # part 1
     pos = list(original_pos)
     total = [0] * 2
     k = 0
     while True:
-        die = (3 * k) % 100 + 1 + (3 * k + 1) % 100 + 1 + (3 * k + 2) % 100 + 1
+        die = (3 * k) % 100 + (3 * k + 1) % 100 + (3 * k + 2) % 100 + 3
         k_mod_2 = k % 2
         pos[k_mod_2] = (pos[k_mod_2] + die) % 10
         total[k_mod_2] += pos[k_mod_2] + 1
@@ -50,6 +53,7 @@ def main(filename):
         k += 1
     print(f"part 1: {result_part_1}")
 
+    # part 2
     wins_a, wins_b = compute_wins(original_pos[0], original_pos[1])
     result_part_2 = max(wins_a, wins_b)
     print(f"part 2: {result_part_2}")
